3f7e31ab8613e20aa1449ac94823fad59320c1f8
Marcos_Neiva <103659504+Marcos1701@users.noreply.github.com>
2023-06-06 15:44:47 +0000

    corrigindo erros da documentação

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: yarn start
+web: yarn start && node ./dist/swagger.js
--- src/swagger.js
+++
@@ -1,6 +0,0 @@
-const swaggerAutogen = require('swagger-autogen')();
-
-const outputFile = './swagger.json';
-const endpointsFiles = ['./api.js'];
-
-swaggerAutogen(outputFile, endpointsFiles);
